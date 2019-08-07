97327807dfe044486acf5ef02c5a92931a66b9ff
chrishsieh <902006+chrishsieh@users.noreply.github.com>
2019-08-07 17:46:39 +0800

    Update sample code.

--- api-win_nodb/src/shared/services/demo.service.ts
+++ api-win_nodb/src/shared/services/demo.service.ts
@@ -8,20 +8,31 @@ export class DemoService {
 	async job(data: DemoDTO) {
 		console.log(data);
 		var decode_data = Base64.atob(data.input_QR);
-		return Object.assign({ status: 'OK' }, data, {
-			SN_Str: decode_data.split('/')[1],
-			UserReadStr:
-				decode_data.split('/')[0] +
-				'/' +
-				decode_data.split('/')[2] +
-				' ' +
-				(Number(decode_data.split('/')[3]) / 1000).toFixed(3).toString() +
-				'pcs',
-			'1D_Barcode':
-				decode_data.split('/')[0] +
-				decode_data.split('/')[2] +
-				decode_data.split('/')[3],
-			Time: new Date(),
-		});
+		var out_data;
+		if (decode_data.split('/').length > 3) {
+			out_data = Object.assign({ status: 'OK' }, data, {
+				SN_Str: decode_data.split('/')[1],
+				UserReadStr:
+					decode_data.split('/')[0] +
+					'/' +
+					decode_data.split('/')[2] +
+					' ' +
+					(Number(decode_data.split('/')[3]) / 1000).toFixed(3).toString() +
+					'pcs',
+				'1D_Barcode':
+					decode_data.split('/')[0] +
+					decode_data.split('/')[2] +
+					decode_data.split('/')[3],
+				Time: new Date(),
+			});
+		} else {
+			out_data = Object.assign({ status: 'OK' }, data, {
+				SN_Str: decode_data,
+				UserReadStr: decode_data,
+				'1D_Barcode': decode_data,
+				Time: new Date(),
+			});
+		}
+		return out_data;
 	}
 }
